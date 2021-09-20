d31fb2adc4cb3f574c80dbeb4e59604fe3e7b733
Yashwant Saiarjun.S.V <86396701+YashwantSaiarjun@users.noreply.github.com>
2021-09-20 21:08:17 +0530

    Create print.js

---
+++ 4_switch/print.js
@@ -0,0 +1,12 @@
+grade='A';
+switch(grade){
+    case 'A': document.write("Grade A");
+    break;
+
+    case 'B': document.write("Grade B");
+    break;
+
+    case 'C': document.write("Grade C");
+    break;
+
+}
