5f6b9c094c8687c194de26b75bea2f3a581db42b
Yashwant Saiarjun.S.V <86396701+YashwantSaiarjun@users.noreply.github.com>
2021-09-21 19:59:17 +0530

    Create print.js

---
+++ For_Loop/print.js
@@ -0,0 +1,12 @@
+var count;
+
+document.write("For Loop");
+document.write("<br/>");
+
+for(count=0;count<10;count++)
+{
+    document.write("current count : "+count);
+    document.write("<br/>");
+}
+
+document.write("Loop End")
