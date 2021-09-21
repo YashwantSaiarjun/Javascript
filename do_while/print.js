102fc278a03a094fce480ee85f9ce6378a30abb9
Yashwant Saiarjun.S.V <86396701+YashwantSaiarjun@users.noreply.github.com>
2021-09-21 19:55:38 +0530

    Create print.js

---
+++ do_while/print.js
@@ -0,0 +1,12 @@
+var count=0;
+
+document.write("Loop Starting");
+document.write("<br/>")
+
+do
+{
+    document.write("count : "+count+"<br/>");
+    count++;
+}while(count<10);
+
+document.write("Loop End")
